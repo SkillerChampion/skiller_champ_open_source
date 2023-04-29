d4b968bb2346062b55d47520ba287374b9159033
SkillerChampion <128652723+SkillerChampion@users.noreply.github.com>
2023-04-28 21:06:11 -0700

    debugging deployment

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const cors = require('cors');
 
@@ -8,16 +7,9 @@ app.use(express.json({ extended: false }));
 
 app.use('/api/fortuneWheel', require('./src/routes/fortuneWheel'));
 
-//Make avail static asset in production
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('build'));
+app.get('/api/health', (req, res) => res.send('App is running'));
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'build', 'server.js'));
-//   });
-// }
-
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
 
 app.listen(PORT, function () {
   console.log(`Server started on PORT ${PORT}`);
